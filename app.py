9611e5b86dfe6d6f5e31cb742dc5a8b09abdcf06
weishc <61315168+weishc@users.noreply.github.com>
2021-02-01 21:48:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,26 @@
 from flask import Flask, request, Response
-import requests, json
+import requests
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return 'yo'
-
 @app.route('/', methods=['POST'])
 def respond():
-    action_type = request['action']['type']
-    proj_name = request['action']['data']['list']['name']
-    shot_name = request['action']['data']['card']['name']
-    short_link = request['action']['data']['card']['shortLink']
+    action_type = request.json['action']['type']
     if action_type == 'commentCard':
-        text = proj_name + '\n' + shot_name + '\nhttps://trello.com/c/' + short_link
         to_workplace()
     return Response(status=200)
 
+
 def to_workplace():
+    proj_name = request.json['action']['data']['list']['name']
+    shot_name = request.json['action']['data']['card']['name']
+    short_link = request.json['action']['data']['card']['shortLink']
+    text = proj_name + r'\n' + shot_name + r'\n⚠️有新留言\nhttps://trello.com/c/' + short_link
     url = 'https://graph.facebook.com/me/messages'
     token = 'DQVJzYXY3eG1RU3NBSEsya05sM2hqOEMwdkM0YkMwN1RSS3JoeWtYM05oRWRNT0JVb0drWEJsX2ZAuYzJJa2wwTWxmY0lqbUNnMElLVUMxMmxuQVhoc09KX2xjUFdNSlhjSl8yZAVhLT1VBUGRia1ROXzdzYVAyc082WFlXWVA5ZAXUxTU00dHR1czFkb21sUkFwUDQ3WDFuX1Vra1l4ZADVOUWtNQThkQU9rdkVLbEpOOGRsc0Q2dkVsbmt2aFhKOS10V2lhbWhn'
-    data = {'recipient': {'id': '100063124216900'}, 'message': {'text': text}}
+    data ='{"recipient":{"id":"100063124216900"},"message":{"text":"'+ text +'"}}'
 
     headers = {
         'Authorization': 'Bearer ' + token,
@@ -33,8 +31,7 @@ def to_workplace():
         'POST',
         url,
         headers=headers,
-        # proxies=proxies,
-        data=str(data)
+        data=data.encode('utf-8')
     )
 
     print(response.text)
