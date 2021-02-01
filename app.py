93e1dada4fce581d9752b873e702e924f024adcb
weishc <61315168+weishc@users.noreply.github.com>
2021-02-01 22:01:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,10 +22,10 @@ def to_workplace():
     proj_name = request.json['action']['data']['list']['name']
     shot_name = request.json['action']['data']['card']['name']
     short_link = request.json['action']['data']['card']['shortLink']
-    text = proj_name + '\n' + shot_name + '\n有未讀留言\nhttps://trello.com/c/' + short_link
+    text = proj_name + r'\n' + shot_name + r'\n⚠️有新留言\nhttps://trello.com/c/' + short_link
     url = 'https://graph.facebook.com/me/messages'
     token = 'DQVJzYXY3eG1RU3NBSEsya05sM2hqOEMwdkM0YkMwN1RSS3JoeWtYM05oRWRNT0JVb0drWEJsX2ZAuYzJJa2wwTWxmY0lqbUNnMElLVUMxMmxuQVhoc09KX2xjUFdNSlhjSl8yZAVhLT1VBUGRia1ROXzdzYVAyc082WFlXWVA5ZAXUxTU00dHR1czFkb21sUkFwUDQ3WDFuX1Vra1l4ZADVOUWtNQThkQU9rdkVLbEpOOGRsc0Q2dkVsbmt2aFhKOS10V2lhbWhn'
-    data = "{\"recipient\": {\"id\": \"100063124216900 \"},\"message\": {\"text\": \""+ text.encode('utf-8') +"\"}}"
+    data ='{"recipient":{"id":"100063124216900"},"message":{"text":"'+ text +'"}}'
 
     headers = {
         'Authorization': 'Bearer ' + token,
@@ -36,8 +36,7 @@ def to_workplace():
         'POST',
         url,
         headers=headers,
-        # proxies=proxies,
-        data=data
+        data=data.encode('utf-8')
     )
 
     print(response.text)
