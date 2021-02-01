b414051d4a0dc82886a51cb7942ed1e6f0400aac
weishc <61315168+weishc@users.noreply.github.com>
2021-02-01 17:31:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,11 @@
-from flask import Flask, request, Response
-import requests
-import json
+from flask import *
 
 app = Flask(__name__)
 
+
 @app.route('/')
-    def hello():
-        return "hello"
+def index():
+    return 'test123'
 
-@app.route('/', methods=['POST'])
-def respond():
-    data = json.dumps(request.json, sort_keys=True,
-                      indent=4, separators=(',', ': '))
-    print(data)
-    to_workplace()
-    return Response(status=200)
-
-
-def to_workplace():
-    text = 'receive'
-    url = 'https://graph.facebook.com/me/messages'
-    token = 'DQVJzYXY3eG1RU3NBSEsya05sM2hqOEMwdkM0YkMwN1RSS3JoeWtYM05oRWRNT0JVb0drWEJsX2ZAuYzJJa2wwTWxmY0lqbUNnMElLVUMxMmxuQVhoc09KX2xjUFdNSlhjSl8yZAVhLT1VBUGRia1ROXzdzYVAyc082WFlXWVA5ZAXUxTU00dHR1czFkb21sUkFwUDQ3WDFuX1Vra1l4ZADVOUWtNQThkQU9rdkVLbEpOOGRsc0Q2dkVsbmt2aFhKOS10V2lhbWhn'
-    data = {'recipient': {'id': '100063124216900'}, 'message': {'text': text}}
-
-    headers = {
-        'Authorization': 'Bearer ' + token,
-        'Content-Type': 'application/json'
-    }
-
-    response = requests.request(
-        'POST',
-        url,
-        headers=headers,
-        data=str(data)
-    )
-
-    print(response.text)
+if __name__ == '__main__':
+    app.run(debug=True)
