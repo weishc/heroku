4c130fb0f9ac4cd09e7d2e931a6eb3d7413bb6e7
weishc <61315168+weishc@users.noreply.github.com>
2021-02-03 01:02:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,105 +3,114 @@ import requests
 import json
 
 app = Flask(__name__)
-wp_staff_db = [{'name': 'test man (yc)', 'id': '100061038601811'}, {'name': 'Wei-Xiang Chen (wc)', 'id': '100063124216900'}]
-headers = {"Accept": "application/json"}
+wptoken = 'DQVJzYXY3eG1RU3NBSEsya05sM2hqOEMwdkM0YkMwN1RSS3JoeWtYM05oRWRNT0JVb0drWEJsX2ZAuYzJJa2wwTWxmY0lqbUNnMElLVUMxMmxuQVhoc09KX2xjUFdNSlhjSl8yZAVhLT1VBUGRia1ROXzdzYVAyc082WFlXWVA5ZAXUxTU00dHR1czFkb21sUkFwUDQ3WDFuX1Vra1l4ZADVOUWtNQThkQU9rdkVLbEpOOGRsc0Q2dkVsbmt2aFhKOS10V2lhbWhn'
+wp_headers = {
+    'Authorization': 'Bearer ' + wptoken,
+    'Content-Type': 'application/json'
+}
+tr_headers = {"Accept": "application/json"}
 query = {
     'key': 'e16b5d87b76092e1854b698a1cc9f465',
     'token': '1594c0a47da4689feb849ef6444572de271cd4ad664a5c46ac33ff31973733c7'
-}  
+}
+tid_wpid = {}
+id_db = {}
+for t in get_trello_staff():
+    for w in get_wp_staff():
+        if t['fullName'] in w['name']:
+            tid_wpid[t['id']] = w['id']
+            id_db[ti['username']] = {
+                'wp_id': w['id'], 'tr_id': t['id']}
+
 
 @app.route('/')
 def index():
-    return 'yo'
+    return '2021/02/02 16:27 update'
 
 
 @app.route('/', methods=['POST'])
 def respond():
-    action_type = request.json['action']['type']
-    request_data = request.json['action']['data']
-
-    if action_type == 'commentCard':
-        card_id = request_data['card']['id']
-        card_members_id = json.loads(get_card_members_id(card_id))
-        card_members_name = []
-
+	req_act = request.json['action']
+    act_type = req_act['type']
+    req_data = req_act['data']
+    card = req_data['card']
+    if act_type == 'commentCard':
+    	commenter = req_act['memberCreator']['username']
+    	shot_user = card['name'].split('] [')[2]
+    	pattern = re.compile(r'@[a-z]+')
+		mention_list = pattern.findall(req_data['text'])
+        card_members_id = json.loads(get_card_members_id(card['id']))
+        if commenter.split('renpic')[1] == shot_user:
+        	card_members_id.remove(id_db[commenter]['tr_id'])
         for i in card_members_id:
-            card_members_name.append(get_fullname(i))
-        send_wpids = []
-
-        for staff in wp_staff_db:
-            mention = '@renpic' + staff['name'].split('(')[1].strip(')')
-            if mention in request_data['text']:
-                send_wpids.append(staff['id'])
-            if staff['name'].split(' (')[0] in card_members_name:
-                send_wpids.append(staff['id'])
+        	send_wp_ids.append(tid_wpid[i])
+        if mention_list != []:
+        	for user in mention_list:
+        		send_wp_ids.append(id_db[user.strip('@')]['wp_id'])
         send_wpids = list(set(send_wpids))
-
         for wpid in send_wpids:
             send_msg(wpid)
-
-    print (json.dumps(request.json, sort_keys=True, indent=4, separators=(",", ": ")))
     return Response(status=200)
 
-
 def send_msg(id):
-    request_data = request.json['action']['data']
-    proj_name = request_data['list']['name']
-    shot_name = request_data['card']['name']
-    short_link = request_data['card']['shortLink']
+    req_data = request.json['action']['data']
+    proj_name = req_data['list']['name']
+    shot_name = req_data['card']['name']
+    short_link = req_data['card']['shortLink']
     text = proj_name + r'\n' + shot_name + r'\n⚠️有新留言\nhttps://trello.com/c/' + short_link
-    wptoken = 'DQVJzYXY3eG1RU3NBSEsya05sM2hqOEMwdkM0YkMwN1RSS3JoeWtYM05oRWRNT0JVb0drWEJsX2ZAuYzJJa2wwTWxmY0lqbUNnMElLVUMxMmxuQVhoc09KX2xjUFdNSlhjSl8yZAVhLT1VBUGRia1ROXzdzYVAyc082WFlXWVA5ZAXUxTU00dHR1czFkb21sUkFwUDQ3WDFuX1Vra1l4ZADVOUWtNQThkQU9rdkVLbEpOOGRsc0Q2dkVsbmt2aFhKOS10V2lhbWhn'
     data ='{"recipient":{"id":"' + id + '"},"message":{"text":"'+ text +'"}}'
-
-    wpheaders = {
-        'Authorization': 'Bearer ' + wptoken,
-        'Content-Type': 'application/json'
-    }
-
     response = requests.request(
         'POST',
         'https://graph.facebook.com/me/messages',
-        headers=wpheaders,
+        headers=wp_headers,
         data=data.encode('utf-8')
     )
-
     print(response.text)
 
 def get_card_members_id(card_id):
     url = "https://api.trello.com/1/cards/"+card_id+'/idMembers'
-
     response = requests.request(
        "GET",
        url,
-       headers=headers,
+       headers=tr_headers,
        params=query,
     )
-
     return response.text
 
-def get_username(id):
-    url = "https://api.trello.com/1/members/" + id + "/username"
-
+def get_unread_list(id)
+    url = "https://api.trello.com/1/members/" + id + "/notifications/"
+    query['read_filter'] = 'unread'
+    query['fields'] = 'data'
+    query['memberCreator_fields'] = ''
     response = requests.request(
        "GET",
        url,
-       headers=headers,
+       headers=tr_headers,
        params=query
     )
+    notif_list = json.loads(response.text)
+    return [i['data']['card']['id'] for i in notif_list]
 
-    return json.loads(response.text)['_value']
-
-def get_fullname(id):
-    url = "https://api.trello.com/1/members/" + id + '/fullName'
+def get_trello_staff():
+    url = "https://api.trello.com/1/organizations/test84104475/members"
 
     response = requests.request(
-       "GET",
-       url,
-       headers=headers,
-       params=query
+        "GET",
+        url,
+        headers=tr_headers,
+        params=query
     )
 
-    return json.loads(response.text)['_value']
+    return json.loads(response.text)
+
+def get_wp_staff():
+    url = 'https://graph.facebook.com/company/organization_members'
+    response = requests.request(
+        'GET',
+        url,
+        headers=wp_headers
+    )
+    return json.loads(response.text)['data']
 
 if __name__ == '__main__':
     app.run(debug=True)
