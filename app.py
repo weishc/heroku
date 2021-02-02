97f5be5b618964e3914e31359a9574182d99dcbe
weishc <61315168+weishc@users.noreply.github.com>
2021-02-02 12:01:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 app = Flask(__name__)
-
+staff_db = [{'name': 'test man', 'id': '100061038601811'}, {'name': 'Wei-Xiang Chen', 'id': '100063124216900'}]
 
 @app.route('/')
 def index():
@@ -14,19 +14,23 @@ def index():
 def respond():
     action_type = request.json['action']['type']
     if action_type == 'commentCard':
-        print (json.dumps(request.json, sort_keys=True, indent=4, separators=(",", ": ")))
-        to_workplace()
+        comment_creator = request.json['action']['"memberCreator']['fullName']
+        receive_staff = ['test man','Wei-Xiang Chen (wc)',comment_creator]
+        for i in staff_db:
+            if i['name'] in receive_staff:
+                send_msg(i['id'])
     return Response(status=200)
 
 
-def to_workplace():
-    proj_name = request.json['action']['data']['list']['name']
-    shot_name = request.json['action']['data']['card']['name']
-    short_link = request.json['action']['data']['card']['shortLink']
+def send_msg(id):
+    request_data = request.json['action']['data']
+    proj_name = request_data['list']['name']
+    shot_name = request_data['card']['name']
+    short_link = request_data['card']['shortLink']
     text = proj_name + r'\n' + shot_name + r'\n⚠️有新留言\nhttps://trello.com/c/' + short_link
     url = 'https://graph.facebook.com/me/messages'
     token = 'DQVJzYXY3eG1RU3NBSEsya05sM2hqOEMwdkM0YkMwN1RSS3JoeWtYM05oRWRNT0JVb0drWEJsX2ZAuYzJJa2wwTWxmY0lqbUNnMElLVUMxMmxuQVhoc09KX2xjUFdNSlhjSl8yZAVhLT1VBUGRia1ROXzdzYVAyc082WFlXWVA5ZAXUxTU00dHR1czFkb21sUkFwUDQ3WDFuX1Vra1l4ZADVOUWtNQThkQU9rdkVLbEpOOGRsc0Q2dkVsbmt2aFhKOS10V2lhbWhn'
-    data ='{"recipient":{"id":"100063124216900"},"message":{"text":"'+ text +'"}}'
+    data ='{"recipient":{"id":"' + id + '"},"message":{"text":"'+ text +'"}}'
 
     headers = {
         'Authorization': 'Bearer ' + token,
@@ -41,6 +45,9 @@ def to_workplace():
     )
 
     print(response.text)
+
+
+
 
 
 if __name__ == '__main__':
